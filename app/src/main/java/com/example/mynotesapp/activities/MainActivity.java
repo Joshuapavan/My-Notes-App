05b4beb851a2fd54891fff0f6cf6d27c051da0fc
Joshuapavan <joshuapavan35@gmail.com>
2021-11-13 18:59:57 +0530

    - Created a Recycler View and an Recycler View Adapter that takes data from the database and displays it in the Recycler View of the main Layout

--- app/src/main/java/com/example/mynotesapp/activities/MainActivity.java
+++ app/src/main/java/com/example/mynotesapp/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mynotesapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -9,14 +11,20 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.example.mynotesapp.R;
+import com.example.mynotesapp.adapters.NotesAdapter;
 import com.example.mynotesapp.database.NotesDatabase;
 import com.example.mynotesapp.entities.Note;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView newNote,addNote,addImage,addLink;
+
+    RecyclerView notesRecyclerView;
+    List<Note> noteList;
+    NotesAdapter notesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +33,18 @@ public class MainActivity extends AppCompatActivity {
 
         initialise();
         newNoteClick(newNote);
+        recyclerView();
     }
+
+
 
     void initialise(){
         newNote = findViewById(R.id.imageAddNoteMain);
         addNote = findViewById(R.id.imageAddNote);
         addImage = findViewById(R.id.imageAddImage);
         addLink = findViewById(R.id.imageAddLink);
+
+        notesRecyclerView = findViewById(R.id.notesRecyclerView);
     }
 
     void newNoteClick(ImageView newNote){
@@ -50,11 +63,31 @@ public class MainActivity extends AppCompatActivity {
                 return NotesDatabase.getDatabase(getApplicationContext()).noteDao().getAllNotes();
             }
 
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
+                if(noteList.size() == 0){
+                    noteList.addAll(notes);
+                    notesAdapter.notifyDataSetChanged();
+                }
+                else{
+                    noteList.add(0,notes.get(0));
+                    notesAdapter.notifyItemInserted(0);
+                }
+                notesRecyclerView.smoothScrollToPosition(0);
             }
         }
         new GetNotesTask().execute();
+    }
+
+    void recyclerView(){
+        notesRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+
+        noteList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(noteList);
+        notesRecyclerView.setAdapter(notesAdapter);
+
+        getNotes();
     }
 }
